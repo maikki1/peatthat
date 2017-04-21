51f2df200e260e432f40276fbddee8aa21ddcfa8
Mai <anna-maija.rauramaa@aalto.fi>
2017-04-21 12:20:01 +0300

    väliupdate

--- js/game.js
+++ js/game.js
@@ -1,6 +1,9 @@
-(function() {
-    var canvas = document.getElementById('canvas'),
-            context = canvas.getContext('2d');
+window.onload = function() {
+
+function fitbrowser() {
+    
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
 
     // resize the canvas to fill browser window dynamically
     window.addEventListener('resize', resizeCanvas, false);
@@ -19,5 +22,26 @@
 
     function drawStuff() {
             // do your drawing stuff here
-    }
-})();
+    }               
+};
+
+fitbrowser();
+
+var game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+function preload() {
+    game.load.image('flower', 'assets/flower.png');
+    game.load.image('triangle', 'assets/triangle-transp.png');
+    game.load.image('circle', 'assets/purple-circle.png');
+    /*game.load.spritesheet('dude', 'assets/dude.png', 32, 48)
+    */;
+}
+    
+function create() {
+    game.add.sprite(0, 0, 'flower');
+}
+function update() {
+}   
+ 
+    
+/* window.onloadin pari, do not touch */    
+}    
