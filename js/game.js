0bed0c11541246175596f7e221dc9be8969b6a18
“tatu <koivisto.tatu@gmail.com>
2017-04-24 14:02:53 +0300

    jotain

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 window.onload = function() {
-    
+
 var game = new Phaser.Game('100%', '100%', Phaser.AUTO, '', { preload: preload, create: create, update: update });
-    
+
 var sprite;
 var weapon;
 var cursors;
@@ -14,28 +14,57 @@ var rocketScale = 0.01;
 var playerScale = 0.05;
 var playerHealth = 3; //debug
 var playAgainButton;
+var testiAttack;
 //var enemyHealth = 15;
- 
+
 function newGameClick() {
     restart();
     //Tänne vois sit esim. lisätä pelaajatilastojen kasvattamista ymsyms.
-}    
+}
 
-// Game Over  
+function createEnemyAttack(frequency, speed, img, strength) {
+  myHits = 0;
+  console.log("1createEnemyAttack: " );
+  enemyAttack = game.add.emitter(game.world.centerX, 0, 200);
+  enemyAttack.width = game.world.width;
+  enemyAttack.makeParticles(img, 200, 200, true, true);
+  enemyAttack.minParticleSpeed.set(-speed, 200);
+  enemyAttack.maxParticleSpeed.set(speed, 200);
+  enemyAttack.minParticleScale = 0.02;
+  enemyAttack.maxParticleScale = 0.02;
+  enemyAttack.bounce.setTo(1, 1);
+  enemyAttack.setAll('body.immovable', true);
+  enemyAttack.strength = strength;
+  this.hitsCounter =  function () {
+      console.log("hits counter +1");
+      myHits += 1;
+      return myHits;
+  };
+  console.log("hits: " + this.hits);
+  this.emitAttack = function () {
+        enemyAttack.start(false, 0, frequency, 1, false);
+        console.log("emit works!!");
+  };
+
+  //enemies.add(enemyAttack); //groupataan kaikki
+}
+
+// Game Over
 function gameOver() {
-    gameoverscreen = game.add.sprite(game.world.centerX - 200, game.world.centerY - 100, 'gameover'); 
+    gameoverscreen = game.add.sprite(game.world.centerX - 200, game.world.centerY - 100, 'gameover');
     gameoverscreen.scale.setTo(0.3);
     weapon.autofire = false;
-    enemies.destroy();
+    //enemies.destroy();
+    enemyAttack.destroy();
     playAgainButton = game.add.button(game.world.centerX - 100, game.world.centerY, 'playagain', newGameClick, this, 2, 1, 0);
-    
-} 
+
+}
 
 function restart() {
     game.state.restart();
 }
-    
-// Create guns       
+
+// Create guns
 // name: 'id' for later use(?), image: img, speed: Num, rate: Num, efficiency: Num, automatic: bool, whoseGun: Sprite
 function createWeapons(name, image, speed, rate, efficiency, automatic, whoseGun) {
   weapon = game.add.weapon(30, image);
@@ -47,13 +76,12 @@ function createWeapons(name, image, speed, rate, efficiency, automatic, whoseGun
   weapon.bulletSpeed = speed;
   weapon.autofire = automatic;
   weapon.fireRate = rate;
-  weapon.bulletSpeedVariance = (weapon.bulletSpeed) / 3;
   weapon.trackSprite(whoseGun, 0, -60);
   var weaponEfficiency = efficiency; //määrittelee, montako kertaa tällä pitää osua
   var weaponID = name;
 }
-    
-// Preload images   
+
+// Preload images
 function preload() {
     game.load.image('flower', 'assets/flower.png');
     game.load.image('triangle', 'assets/triangle-transp.png');
@@ -63,88 +91,97 @@ function preload() {
     game.load.image('gameover', 'assets/gameover.png');
     game.load.image('playagain', 'assets/playagain.png');
 }
-    
 
-// New game default setup    
+
+// New game default setup
 function create() {
 
+  testiAttack = new createEnemyAttack(1000, 1000, 'enemyAttack', 5);
+  console.log("1testiAttack: " + testiAttack);
+  testiAttack.emitAttack();
+  console.log("2testiAttack: " + testiAttack);
+
+
   game.physics.startSystem(Phaser.Physics.ARCADE);
-    
-  // Ajattelin, että nämä kaikki kuitenkin on defaulttina jokaisessa pelissä, joten voivat olla create -funktiossa.    
+
+  // Ajattelin, että nämä kaikki kuitenkin on defaulttina jokaisessa pelissä, joten voivat olla create -funktiossa.
 
   // Home base
   platforms = game.add.physicsGroup();
   platforms.create(0, game.world.height - 120, 'land');
   platforms.setAll('body.immovable', true);
-    
-  // Player    
+
+  // Player
   sprite = this.add.sprite(game.world.centerX, game.world.height - 200, 'triangle');
   sprite.anchor.set(0.5);
   sprite.scale.setTo(playerScale);
-  game.physics.arcade.enable(sprite);    
+  game.physics.arcade.enable(sprite);
 
   sprite.inputEnabled = true;
   sprite.input.allowVerticalDrag = false;
   sprite.input.enableDrag();
   sprite.body.collideWorldBounds = true;
-    
-    
+
+
   // Create a gun or two
   //@param1 name, @param2 img, @param3 speed, p4 freq, p5 efficiency, p6 autoplay, p7 whose
   createWeapons('default', 'circle', 900, 120, 8, true, sprite);
   //createWeapons('rocket', 'flower', 200, 500, 2, false, sprite);
 
-  //"Event-listener-stuff", ie. listening to key-events for shooting.    
+  //"Event-listener-stuff", ie. listening to key-events for shooting.
   cursors = this.input.keyboard.createCursorKeys();
-    
-  fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR, Phaser.KeyCode.SPACEBAR);  
+
+  fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR, Phaser.KeyCode.SPACEBAR);
   rocketButton = this.input.keyboard.addKey(Phaser.KeyCode.Q, Phaser.KeyCode.Q);    //Jos double-tap vastaisi tätä?
 
-    
-    
-  // Default setup stuff    
+
+
+  // Default setup stuff
   game.stage.backgroundColor = '#B6E4CC';
   this.game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
-    
-   //...including enemy attacks 
-  enemies = game.add.group();
-  createEnemyAttack(1000, 1000, 'enemyAttack', 5); //@param1 frequency @param2 speed @p3 img @p4 strength (shots needed to kill)  
-  createEnemyAttack(5000, 500, 'flower');  
+
+   //...including enemy attacks
+//  enemies = game.add.group();
+//  createEnemyAttack(1000, 1000, 'enemyAttack', 5); //@param1 frequency @param2 speed @p3 img @p4 strength (shots needed to kill)
+//  createEnemyAttack(5000, 500, 'flower');
   //Nyt voi luoda useamman erilaisen enemyn.
   //Näitä modaamalla tasojen vaikeustason vaihtelu helppoa.
 }
-  
-    
+
+
 /** Täältä...
- */ 
+ */
     //@p1 enemy, @p2 sprite's bullet (?)
 function bulletEnemyAttackCollision(first, second) {
     second.kill();
-    first.hits += 1;
-    console.log(first.hits + " enemy's hits")
+    console.log("first: " + testiAttack.hits);
+    testiAttack.hitsCounter();
+    console.log("first.strength: " + first.strength);
+    console.log(first.hits + " second enemy's hits");
     if(first.hits == first.strength) {
       first.kill();
     }
 }
 
-// @param1 - ??, @param2 enemy ?  
-function enemyAttackHit(first, second) { 
+// @param1 - ??, @param2 enemy ?
+function enemyAttackHit(first, second) {
     playerHealth -= 1; //Aim: playerHealth vähenee riippuen enemyn tyypistä; aika intuitiivista.
     first.kill();
     console.log("You got hit!");
     console.log(playerHealth + " health");
 }
-    
+
 /*  ..tänne.
- */ 
-   
+ */
+
 
 function update() {
+  console.log("3testiAttack: " + testiAttack);
+  //console.log("enemyAttack: " + enemyAttack);
+  game.physics.arcade.collide(enemyAttack, platforms, enemyAttackHit, false, this);
+  game.physics.arcade.collide(enemyAttack, weapon.bullets, bulletEnemyAttackCollision, false, this);
 
-  game.physics.arcade.collide(enemies, platforms, enemyAttackHit, false, this);
-  game.physics.arcade.collide(enemies, weapon.bullets, bulletEnemyAttackCollision, false, this);
-    
-  sprite.body.velocity.x = 0;    
+  sprite.body.velocity.x = 0;
 
   if(cursors === null) {
     console.log("cursors null");
@@ -152,7 +189,7 @@ function update() {
     if(cursors.left.isDown) {
         sprite.body.velocity.x = -800;
     }
-      
+
     else if(cursors.right.isDown) {
         sprite.body.velocity.x = 800;
     }
@@ -169,25 +206,10 @@ function update() {
 
   if(playerHealth < 1) {
       gameOver();
-  } 
+  }
 }
 
-function createEnemyAttack(frequency, speed, img, strength) {
-  enemyAttack = game.add.emitter(game.world.centerX, 0, 200);
-  enemyAttack.width = game.world.width;
-  enemyAttack.makeParticles(img, 200, 200, true, true);
-  enemyAttack.minParticleSpeed.set(-speed, 200);
-  enemyAttack.maxParticleSpeed.set(speed, 200);
-  enemyAttack.minParticleScale = 0.02;
-  enemyAttack.maxParticleScale = 0.02;
-  enemyAttack.bounce.setTo(1, 1);
-  enemyAttack.setAll('body.immovable', true);
-  enemyAttack.strength = strength;
-  enemyAttack.hits = 0;
 
-  enemyAttack.start(false, 0, frequency);
-  enemies.add(enemyAttack); 
-}
 
 /* window.onloadin pari, do not touch */
 };
